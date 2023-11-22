26d02f71521ac9e8597a7b0d6ee404c38c41324f
mianni <71703420+annsperkach@users.noreply.github.com>
2023-11-22 13:41:55 +0100

    chatgpt bot

---
+++ lab6-web-basics/chatGPT-Telegram-bot/init_setup.sh
@@ -0,0 +1,6 @@
+echo [$(date)]: "START"
+echo [$(date)]: "Creating conda env with python 3.8" # change py version as per your need
+conda create --prefix ./env python=3.8 -y && conda activate ./env
+echo [$(date)]: "installing the requirements" 
+pip install -r requirements.txt
+echo [$(date)]: "END"
